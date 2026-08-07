814c91e49dee4e2ee4db1e244daadaf9b022c166
nilolic <nilolic22@gmail.com>
2026-08-07 21:12:59 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,1 +1,15 @@
-const nextConfig = {}; export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [{ type: 'host', value: 'www.tableboost.app' }],
+        destination: 'https://tableboost.app/:path*',
+        permanent: true,
+      },
+    ];
+  },
+};
+
+export default nextConfig;
